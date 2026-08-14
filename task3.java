a69c49e7fde51da66f67c63a4537e3ab054ee4f9
venkatasaivempalla <venkatasaivempalla@gmail.com>
2026-08-14 11:28:44 +0530

    Add files via upload

---
+++ task3.java
@@ -0,0 +1,59 @@
+
+import java.util.*;
+import java.util.stream.*;
+
+public class task3 {
+
+    static class Result {
+        int current;
+        int maximum;
+
+        Result(int current, int maximum) {
+            this.current = current;
+            this.maximum = maximum;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List<Integer> profits = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            profits.add(sc.nextInt());
+        }
+
+        Result result = profits.stream()
+                .skip(1)
+                .reduce(
+                    new Result(
+                        profits.get(0),
+                        profits.get(0)
+                    ),
+                    (r, value) -> {
+
+                        r.current = Math.max(
+                            value,
+                            r.current + value
+                        );
+
+                        r.maximum = Math.max(
+                            r.maximum,
+                            r.current
+                        );
+
+                        return r;
+                    },
+                    (r1, r2) -> r1.maximum > r2.maximum ? r1 : r2
+                );
+
+        System.out.println(result.maximum);
+    }
+}
+
+
+
+
